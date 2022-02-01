91e7f558ecf6cfd8f650b39906d5610eea16c711
Szymon Jankowski <s.jankowski@fireup.pro>
2022-02-01 14:44:18 +0100

    Rozwiązanie zadania

--- 3TIE_gim/js/main.js
+++ 3TIE_gim/js/main.js
@@ -2,16 +2,41 @@
     Zadanie:
     - Wyświetl w czacie nick podany na górze strony
     - Po wpisaniu wiadomości wyczyść pole tekstowe
-    - Jeśli nie podano nicku, a użytkownik próbuje wyłać wiadomość,
+    - Jeśli nie podano nicku, a użytkownik próbuje wysłać wiadomość,
     powinien zobaczyć błąd
-    - Jeśli nie podano nicku, albo pole od wiadomościu jest puste,
+    - Jeśli nie podano nicku, albo pole od wiadomości jest puste,
     przycisk wyślij ma być wyszarzony
 */
 
 const chat = document.getElementById("chat");
 const message = document.getElementById("message");
+const nickinput = document.getElementById("nickinput");
+const btn = document.getElementById("btn");
+
+nickinput.oninput = function() {
+    sprawdzCzyMoznaWyslac();
+};
+
+message.oninput = function() {
+    sprawdzCzyMoznaWyslac();
+};
 
 function send() {
     const msg = message.value;
-    chat.innerHTML += `<div><b>You</b>: ${msg}</div>`;
+    const nick = nickinput.value;
+
+    if(!nick) {
+        chat.innerHTML += `<div><b style="color: red">Nie podano nicku!</b></div>`;    
+    }
+    else {
+        chat.innerHTML += `<div><b>` + nick + `</b>: ${msg}</div>`;
+        message.value = "";
+        sprawdzCzyMoznaWyslac();
+    }
 }
+
+function sprawdzCzyMoznaWyslac() {
+    btn.disabled = !message.value || !nickinput.value;
+}
+
+sprawdzCzyMoznaWyslac();
