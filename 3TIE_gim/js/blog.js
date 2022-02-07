e92550c1869e42cf4422ee2fd84c092b2bd05335
Szymon Jankowski <s.jankowski@fireup.pro>
2022-02-07 13:21:35 +0100

    Removing posts

--- 3TIE_gim/js/blog.js
+++ 3TIE_gim/js/blog.js
@@ -1,23 +1,61 @@
 const titleInput = document.getElementById("title");
 const contentInput = document.getElementById("content");
 const addButton = document.getElementById("add-button");
-const posts = document.getElementById("posts");
+const postContainer = document.getElementById("posts");
 const form = document.getElementById("form");
 const showFormBtn = document.getElementById("show-form");
+
+let posts = [
+    {
+        title: "Tytuł posta",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur perferendis doloribus modi obcaecati tempora, officiis eos atque. Laborum perspiciatis provident laboriosam mollitia dolorum vitae itaque, officiis necessitatibus nostrum qui. Suscipit. Expedita, assumenda. Sint hic voluptatibus reiciendis nostrum nesciunt id, voluptas pariatur deserunt sed necessitatibus itaque quod iusto vero, accusamus illo minus veritatis dolor consequuntur qui expedita, tenetur magni ex? Provident!",
+        date: new Date().toLocaleString("pl"),
+    },
+];
+
+function renderPosts() {
+    postContainer.innerHTML = "";
+
+    for(const post of posts) {
+        if(post != posts[0]) {
+            postContainer.innerHTML += "<hr>";
+        }
+
+        postContainer.innerHTML += `
+            <div class="post">
+                <div class="title">${post.title}</div>
+                <div class="date">
+                    ${post.date}
+                    <a href="#" style="vertical-align: middle" data-id="${post.title}" onclick="deletePost(this)">
+                        <img height="16" src="https://upload.wikimedia.org/wikipedia/commons/8/8f/Flat_cross_icon.svg" alt="">
+                    </a>
+                </div>
+                <div class="post-content">${post.content}</div>
+            </div>
+        `;
+    }
+}
+
+renderPosts();
 
 addButton.onclick = () => {
     const title = titleInput.value;
     const content = contentInput.value;
 
-    posts.innerHTML += `
-        <hr>
-        <div class="post">
-            <div class="title">${title}</div>
-            <div class="date">${new Date().toLocaleString("pl")}</div>
-            <div class="post-content">${content}</div>
-        </div>
-    `;
+    posts.push({
+        title: title,
+        content: content,
+        date: new Date().toLocaleString("pl"),
+    });
+
+    renderPosts();
 };
+
+function deletePost(link) {
+    const id = link.getAttribute("data-id");
+    posts = posts.filter(post => post.title != id);
+    renderPosts();
+}
 
 showFormBtn.onclick = () => {
     if(form.classList.contains("hidden")) {
