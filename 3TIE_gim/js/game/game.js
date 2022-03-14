5e139e73318ba2705178c0518853d206bc8d538f
Szymon Jankowski <s.jankowski@fireup.pro>
2022-03-14 13:24:27 +0100

    Show number of bought items

--- 3TIE_gim/js/game/game.js
+++ 3TIE_gim/js/game/game.js
@@ -25,6 +25,11 @@ function addPoints(number) {
     pointsLabel.innerHTML = Math.round(points);
 }
 
+function setPointsPerSecond(value) {
+    pointsPerSecond = value;
+    perSecondLabel.innerHTML = Math.round(value);
+}
+
 // Timer for adding points per second,
 // called 10 times per second, to make a smooth animation.
 setInterval(() => {
@@ -45,7 +50,10 @@ function save() {
 
 function load() {
     console.log("Loading game from local storage");
-    points = parseInt(localStorage.getItem("points"));
+    let savedPoints = localStorage.getItem("points");
+    if (savedPoints) {
+        points = parseInt(savedPoints);
+    }
 }
 
 // Load game state when script starts
@@ -59,28 +67,61 @@ const items = [{
         name: "Generator ciastek",
         price: 100,
         pointsPerSecond: 1,
-        image: "cookie.png"
+        image: "cookie.png",
+        bought: 0,
     },
     {
         name: "Ciasteczkowy potwór",
         price: 500,
         pointsPerSecond: 10,
-        image: "monster.png"
+        image: "monster.png",
+        bought: 0,
     },
 ];
 
 function renderShopItems() {
+    // Clear current items to prevent duplication
+    itemsContainer.innerHTML = "";
+
+    let index = 0;
     for (const item of items) {
         itemsContainer.innerHTML += `
-<div class="item">
+<div class="item" onclick="onItemClick(items[${index}])">
     <img class="image" src="${item.image}" alt="">
-    <div class="name">${item.name}</div>
+    <div class="name">${item.name} (${item.bought})</div>
     <div class="price">${item.price} cookies</div>
 </div>
 `;
+        index++;
     }
 }
 
 renderShopItems();
+
+function onItemClick(item) {
+    console.log("Buying item: ", item);
+
+    if (points < item.price) {
+        alert("You don't have enough cookies!");
+        return;
+    }
+
+    // alert(`You bought ${item.name}!`);
+
+    item.bought++;
+    points -= item.price;
+    setPointsPerSecond(pointsPerSecond + item.pointsPerSecond);
+    item.price += Math.round(item.price * 0.1);
+
+    renderShopItems();
+}
+
+//#endregion
+
+//#region Debug functions
+
+function resetPoints() {
+    addPoints(-points);
+}
 
 //#endregion
