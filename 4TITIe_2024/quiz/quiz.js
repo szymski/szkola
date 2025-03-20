93e3d37affe3d014d89132ebc618de93b4d0495c
Szymon Jankowski <szymskipl@gmail.com>
2025-03-20 13:33:00 +0100

    Quiz template update

--- 4TITIe_2024/quiz/quiz.js
+++ 4TITIe_2024/quiz/quiz.js
@@ -1,23 +1,48 @@
-const obiekt = {
-    pytanie: "Ile to jest 2+2?",
-    odpowiedzi: [
-        "3",
-        "4",
-        "2",
-        "5",
-    ],
-    poprawna: 1,
-
-    // To samo co obiekt.print = function() {}
-    print() {
-        console.log(this.pytanie);
-
-        for(const odp of this.odpowiedzi) {
-            console.log(odp);
-        }
+const questions = [
+    {
+        title: "Która gwiazda jest najbliżej Ziemi?",
+        answers: ["Słońce", "Proxima Centauri", "Alfa Centauri", "VY Canis Majoris"],
+        correct: 0,
+    },
+    {
+        title: "Ile to 2+2?",
+        answers: ["3", "4", "5", "6"],
+        correct: 1,
+    },
+    {
+        title: "Która planeta jest największa?",
+        answers: ["Merkury", "Wenus", "Mars", "Jowisz"],
+        correct: 3,
     }
+];
+
+const html = {
+    questionNumber: document.getElementById("question-number"),
+    question: document.getElementById("question"),
+    answers: [
+        document.getElementById("answer1"),
+        document.getElementById("answer2"),
+        document.getElementById("answer3"),
+        document.getElementById("answer4"),
+    ],
 };
 
-console.log(obiekt);
+let state = {
+    currentQuestion: 0,
+    question: null,
+}
 
-obiekt.print();
+function onAnswerClick() {
+    alert(123);
+}
+
+function loadQuestion(id) {
+    state.currentQuestion = id;
+    state.question = questions[id];
+    html.question.innerText = state.question.title;
+    for (let i = 0; i < 4; i++) {
+        html.answers[i].innerText = state.question.answers[i];
+    }
+}
+
+loadQuestion(0);
